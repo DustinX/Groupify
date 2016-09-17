ad3675650d906e37de1ff200810ff2188caaf844
Hubby <dustin.dehaven@gmail.com>
2016-09-17 16:03:15 -0700

    Update Grouping algorithm.js

--- Grouping algorithm.js
+++ Grouping algorithm.js
@@ -1,419 +1,550 @@
-//no protect
+
+window.onload = createScores();
+
+function createScores() {
+	//for each person randomly create all 7 scores
+
+	//creating array manually here so that names are consistent.
+
+	var raw = [{
+		id: "Dan",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "Joe",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "Dustin",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "Steve",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "David",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "Jean",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "Neil",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "Jack",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "Stephanie",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "Kenny",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "Matt",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}, {
+		id: "Ken",
+		adventurous: NaN,
+		charismatic: NaN,
+		mellow: NaN,
+		rational: NaN,
+		reliable: NaN,
+		social: NaN,
+		thoughtful: NaN
+	}];
 
 
+	var ungrouped = [{
+		id: "Dan",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "Joe",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "Dustin",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "Steve",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "David",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "Jean",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "Neil",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "Jack",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "Stephanie",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "Kenny",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "Matt",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}, {
+		id: "Ken",
+		L: NaN,
+		S: NaN,
+		C: NaN,
+		W: NaN
+	}];
+
+	//alphabetize lists
+	raw.sort((a, b) => a.id.localeCompare(b.id));
+	ungrouped.sort((a, b) => a.id.localeCompare(b.id));
+
+	//condense native 7 Traitify traits to our 4 traits
+	raw.forEach(function(person, index) {
+		for (let skill in person) {
+			if (skill !== "id") {
+				person[skill] = Math.floor(Math.random() * 100);
+			}
+		}
+
+		ungrouped[index].L = Math.floor((person.adventurous + person.charismatic) / 2);
+		ungrouped[index].S = Math.floor((person.thoughtful + person.social) / 2);
+		ungrouped[index].C = Math.floor((person.rational + person.mellow) / 2);
+		ungrouped[index].W = person.reliable;
+	});
+
+	//display the raw data in a table
+
+	var rawTable = document.getElementById("rawTable");
+
+	raw.forEach(function(object) {
+		let row = document.createElement("tr");
+		for (let skill in object) {
+			let item = document.createElement("td");
+			item.innerText += object[skill];
+			row.appendChild(item);
+		}
+		rawTable.appendChild(row);
+	});
 
 
-function createGroups(array) {
+	//display combined data in a table
 
-  // console.log(array);
+	var ungroupedTable = document.getElementById("ungroupedTable");
 
-  var groups = Math.ceil(array.length / 4);
+	ungrouped.forEach(function(object) {
 
-  //make the array the size of group
-  var answer = [];
-
-  var leftover = [];
-
-  for (i = 0; i < groups; i++) {
-    answer.push({
-      L: "",
-      S: "",
-      C: "",
-      W: ""
-    });
-  }
-
-  //returns an array of id's of the specialists
-  //these people should be placed first in the outer function
-  var specialists = findSpecialists(array);
-  array = specialists[1];
-  specialists = specialists[0];
-  // console.log(array);
-  /// console.log(specialists);
-  leftover = addToGroup(specialists, answer);
+		let row = document.createElement("tr");
+		for (let skill in object) {
+			let item = document.createElement("td");
+			item.innerText += object[skill];
+			row.appendChild(item);
+		}
+		ungroupedTable.appendChild(row);
+	});
 
 
-  //add generalists last to fill in the empty slots
-  //but figure out generalists after specialists and before mixed
-  //anyone not a specialist or generalist is mixed
-  var generalists = findGeneralists(array);
-  array = generalists[1];
-  generalists = generalists[0];
+	//assign people to groups
+	createGroups(ungrouped);
+}
 
-  //console.log(generalists);
-  //console.log(leftover);
+function createGroups(ungrouped) {
 
-  // console.log(generalists);
-  generalists.push(...leftover);
-  //console.log(generalists);
-  
-  //Mixed is who is left over in array at this point
-  //add them in effective order
-  assignMixed(array);
+	//the number of groups we will make
+	var groups = Math.ceil(ungrouped.length / 4);
 
-  //just add the rest of the people in any open slots
-  addRest(generalists, answer);
+	//this will be the created groups we produce
+	var answer = [];
 
+	//make the array the size of group
+	for (let i = 0; i < groups; i++) {
+		answer.push({
+			L: "",
+			S: "",
+			C: "",
+			W: ""
+		});
+	}
 
+	//assign leaders
+	assignLeaders(ungrouped, answer, groups);
 
+	//assign everyone else
+	assignRest(ungrouped, answer, groups);
 
-  //console.log(answer);
-  
-  displayGroups(answer);
+	//display groups on webpage
+	displayGroups(answer);
 
-  return answer;
-
+	return;
 }
 
 
-function findSpecialists(array) {
+function assignLeaders(ungrouped, answer, groups) {
 
-  let dood = [];
+	/*
+    find the people with the highest leader traits
+    assign each of them to their own group
 
-  //console.log(array);
+    this is to make sure each group has a leader, and to
+		avoid having two strong leaders in a group that might argue over direction and control
 
-  //go through each element in array and if there is a person with 3 scores 3 or less and one score 5 or more, save the index of that person to array of specialists.
+    Algorithm:
+		while there are still answer groups with no leader
+    find the person with the highest leader trait
+    send them to assign person
+		*/
 
-  array.forEach(function(person, index) {
-    //console.log(person.L);
+	ungrouped.sort((a, b) => b.L - a.L);
 
-    let lessThan = 0;
-    let greaterThan = 0;
-    let skill = "";
-    //less than is count of skills 3 or less
-    //greater than is count of skills 5 or greater
+	var addThese = [];
 
-    //if lessThan is equal to 3 and greaterThan is equal to 1, that person is a specialist
+	//find a leader for each group
+	for (var i = 0; i < groups; i++) {
 
-    //console.log(person.id);
+		//take the first person off the sorted array
+		let temp = ungrouped.shift();
+		addThese.push({
+			id: temp.id,
+			specialty: "L"
+		});
+	}
 
-    if (person.L <= 30)
-      lessThan++;
-    if (person.S <= 30)
-      lessThan++;
-    if (person.C <= 30)
-      lessThan++;
-    if (person.W <= 30)
-      lessThan++;
+	addToGroup(addThese, answer);
 
-    if (person.L >= 50) {
-      greaterThan++;
-      skill = "L";
-    }
+	return;
+}
 
-    if (person.S >= 50) {
-      greaterThan++;
-      skill = "S";
-    }
-    if (person.C >= 50) {
-      greaterThan++;
-      skill = "C";
-    }
-    if (person.W >= 50) {
-      greaterThan++;
-      skill = "W";
-    }
+function assignRest(ungrouped, answer, groups) {
+	/*
+	Look for the skill that has the fewest people that are strong in that skill
+	Then, assign the person strongest in that skill
 
-    if (lessThan === 3 && greaterThan === 1) {
-      //add to return array
-      dood.push({
-        id: person.id,
-        specialty: skill
-      });
+	Example:
+	Number of people with respective skill below 30:
+	L: (leaders ignored here because they were already assigned in previous function)
+	S: 5
+	C: 4
+	W: 2
 
-      //remove from pool
-      array[index] = null;
-    }
-  });
+	We would place a person with a strong "S" skill here since more ungrouped people have low S skills
+	In this case, we don't want to run out of people with
+	 relatively strong S skills by placing them somewhere else first.
+	 In other words, we don't want to squander to few ungrouped people left with strong S skills.
 
-  //get rid of elements that are now null
+	These numbers are evaluated again after a person is placed.
+	The "threshold" is increased until there are more people below the threshold than there are
+		"extra" people that *wouldn't* need to be that skill.
+		(The "threshold" is a number that is slowly incremented to find the skill with the most people below it,
+	and therfore find the skill with the most "lowskill" people remaining)
 
-  array = array.filter((val) => val !== null);
+	Example:
+	Number of people needed for each skill:
+	L: 0
+	S: 3
+	C: 1
+	W: 2
 
-  //  console.log(array);
+	Number of ungrouped people:
+	6
 
-  //console.log(dood);
+	Number of people that wouldn't have to be each skill:
+	L: 6
+	S: 3
+	C: 5
+	W: 4
 
-  //janky AF
-  return [dood, array];
+	Number of people below the current skill threshold:
+	L: 3
+	S: 4
+	C: 1
+	W: 2
+
+	It is okay that 2 people is blow the threshold for W because there are still
+	 4 people that wouldn't need to cover a W slot
+	However, it is a problem that 4 S's are below the threshold because we still need 3 more S's.
+	So we assign an S next in this case, since they are in demand the most.
 
 
+	After placing the S person, it could be W that has the shortage next.
+	This way we can avoid a skill not getting any strong representitives
+	*/
+
+	//the number of people that wouldn't have to fill that role
+	var excess = {
+		L: 0,
+		S: 0,
+		C: 0,
+		W: 0
+	};
+
+	//the number of empty spots in answer array for that role
+	var needed = {
+		L: 0,
+		S: groups,
+		C: groups,
+		W: groups
+	};
+
+	//the number of people with the skill below the threshold
+	//these are in reverse order of priority so that the highest priority ones overwrite and win out
+	var lowSkill = {
+		S: 0,
+		C: 0,
+		W: 0,
+		L: 0
+	};
+
+	//how many people are below the threshold in that pass???
+	var deficit = {
+		L: 0,
+		S: 0,
+		C: 0,
+		W: 0
+	};
+
+	while (ungrouped.length) {
+
+		excess = {
+			L: 0,
+			S: 0,
+			C: 0,
+			W: 0
+		};
+
+		//to calculate excess = number of people remaining - needed for that skill
+		for (let skill in excess) {
+			excess[skill] = ungrouped.length - needed[skill];
+		}
+
+		var threshold = 10;
+		var deficitSkills = 0;
+		var deficitSkill = "";
+		var greatestShort = 0;
+
+		//find skill with fewest strong representitives
+		while (deficitSkills === 0) {
+
+			//these are in reverse order of priority so that the highest priority ones overwrite and win out
+			//the number of people with the skill below the threshold
+			lowSkill = {
+				S: 0,
+				C: 0,
+				W: 0,
+				L: 0
+			};
+
+			//how many people are below the threshold in that pass
+			deficit = {
+				L: 0,
+				S: 0,
+				C: 0,
+				W: 0
+			};
+
+			//find the number of people below the threshold
+			ungrouped.forEach(function(person) {
+				for (let skill in person) {
+					if (person[skill] < threshold && needed[skill] > 0) {
+						lowSkill[skill]++;
+					}
+				}
+			});
+
+			//check each skill for a deficit at current threshold
+			for (let skill in lowSkill) {
+
+				//if deficit
+				if (lowSkill[skill] >= excess[skill]) {
+
+					//record how many short we are, to break ties if two skills below deficit
+					deficit[skill] = lowSkill[skill] - excess[skill];
+
+					//determine skill with greatest deficit
+					if (deficit[skill] >= greatestShort) {
+						greatestShort = deficit[skill];
+						deficitSkill = skill;
+					}
+
+					deficitSkills++;
+				}
+			}
+
+			//if deficitSkills is still 0, then run the loop again
+
+			//increase the threshold so will find more lowskill in next loop, if needed
+			threshold += 10;
+		}
+
+		//selct the person with the highest skill in the deficit and prepare their data and then send it to the assign function
+
+		let strongestPerson = NaN;
+		let strongestValue = 0;
+
+		ungrouped.forEach(function(person, index) {
+			if (person[deficitSkill] > strongestValue) {
+				strongestValue = person[deficitSkill];
+				strongestPerson = index;
+			}
+		});
+
+		//decrement needed object for the skill we just placed someone for
+		needed[deficitSkill]--;
+
+		//prepare the data for the seleted person and call the assign function
+		var addThem = [];
+
+		//[{id: "name", skill: "L"}]
+		addThem.push({
+			id: ungrouped[strongestPerson].id,
+			specialty: deficitSkill
+		});
+
+		//remove them from the candidate array
+		ungrouped.splice(strongestPerson, 1);
+
+		//add to group
+		addToGroup(addThem, answer);
+	}
 }
 
 
-function assignMixed(array){
-  
-  
-  //identify shortage groups
-  //assign to shortage startingn from person with highest proficiency
-  //anyone left over just give to generalists, since their specialty can't be used
-  
-  
-  
-  
-  
-}
-
-
-function findGeneralists(array) {
-
-  //changing this for demo purposes to include everyone else
-  //normally the mixed group would have its own function
-
-  //noprotect
-
-  var dood = [];
-
-  //find median
-  //if no skill is outside 15 of median, they are generalist.  Add to return array
-
-  array.forEach(function(object, index) {
-
-
-    let values = [];
-
-    //get the scores from the object
-    for (let property in object) {
-      //if not the person's name
-      if (property !== "id") {
-        values.push(object[property]);
-      }
-    }
-
-    //find median
-    values.sort((a, b) => a - b);
-    let lowMiddle = Math.floor((values.length - 1) / 2);
-    let highMiddle = Math.ceil((values.length - 1) / 2);
-    let median = (values[lowMiddle] + values[highMiddle]) / 2;
-
-
-    //if none of the values are more than 15 away from the median
-    if (!values.some((val) => Math.abs(val - median) > 115)) {
-
-      //add to return array
-      dood.push({
-        id: object.id,
-        specialty: ""
-      });
-
-      //remove from pool
-      array[index] = null;
-    }
-
-
-
-  });
-
-  //console.log(dood);
-
-  array = array.filter((val) => val !== null);
-
-
-  return [dood, array];
-
-}
-
-//mixed is everything else
-
-
-//[{id: "name", skill: "L"}]
-
-//addToGroup can return an array of one or many
+//addToGroup can accept an array of one or many
 //if many, just go through it an add each one
-function addToGroup(arrayAdd, answer) {
+function addToGroup(ungroupedAdd, answer) {
 
-  //for each person in the array, find a team with that skill empty and add person
-  var index = 0;
-  var leftover = [];
+	//for each person in the array, find a team with that skill empty and add person
+	var index = 0;
 
-  //  console.log(arrayAdd);
+	ungroupedAdd.forEach(function(person, curr) {
 
-  arrayAdd.forEach(function(object, curr) {
-    //go through answer array and find first team with an empty slot for the skill of that person.  Add them to that slot.
-    index = answer.findIndex((answerObject) => answerObject[object.specialty] === "");
+		//find first team with an empty slot for the skill of that person, then add them there
+		index = answer.findIndex((answerObject) => answerObject[person.specialty] === "");
+		answer[index][person.specialty] = person.id;
+	});
 
-    if (index < 0) {
-      leftover.push(arrayAdd[curr]);
-    } else {
-
-      answer[index][object.specialty] = object.id;
-    }
-  });
-
-  //if there is no space for the rest of the specialists, return what is left of the area and add these people to generalists
-
-
-  //handle cases where group size is not multiple of 4
-
-  return leftover;
-
+	return;
 }
 
 
-function addRest(arrayAdd, answer) {
-
-  debugger;
-
-  // console.log(arrayAdd);
-  //for each person in the array, find a team with that skill empty and add person
-  var index = 0;
-  var specialty = "";
-
-  //  console.log(arrayAdd);
-
-  arrayAdd.forEach(function(object, curr) {
-    //go through answer array and find first team with an empty slot for the skill of that person.  Add them to that slot.
-    index = answer.findIndex(function(answerObject) {
-      for (let foo in answerObject) {
-        if (answerObject[foo] === "") {
-          specialty = foo;
-          return true;
-        }
-      }
+function displayGroups(answer) {
+	/*
+	  loop through answer array
+	    create div for each group
+	  loop through object
+	    add each name to div
+	*/
 
 
-    });
+	var holderAnswer = document.createElement("div");
 
-    if (index < 0) {
-      //
-    } else {
+	answer.forEach(function(object, index) {
+		let group = document.createElement("div");
+		group.innerText = "group " + (index + 1) + "\n";
+		for (let skill in object) {
+			group.innerText += skill + ": " + object[skill] + "\n";
+		}
+		group.innerText += "\n";
+		holderAnswer.appendChild(group);
+		holderAnswer.id = "answer";
+	});
 
-      answer[index][specialty] = object.id;
-    }
-  });
+	document.body.appendChild(holderAnswer);
 
-
+	return;
 }
-
-function displayGroups(answer){
-  /*
-    loop through answer array
-      create div for each group
-    loop through object
-      add each name to div
-  */
-  
-  
-  var holder = document.createElement("div");
-  
-  answer.forEach(function(object, index){
-    let group = document.createElement("div");
-    group.innerText = "Group " + (index + 1) + "\n";
-    for(let skill in object){
-      group.innerText += skill + ": " + object[skill] + "\n";
-    }
-    group.innerText += "\n";
-    holder.appendChild(group);
-    
-  })
-  
-  document.body.appendChild(holder);
-  
-  
-  
-}
-
-
-
-
-//console.log(
-  createGroups(
-    [{
-        id: "Dan",
-        L: 40,
-        S: 30,
-        C: 80,
-        W: 20
-      }, {
-        id: "Joe",
-        L: 30,
-        S: 90,
-        C: 80,
-        W: 40
-      }, {
-        id: "Dustin",
-        L: 10,
-        S: 30,
-        C: 80,
-        W: 50
-      }, {
-        id: "Steve",
-        L: 60,
-        S: 10,
-        C: 80,
-        W: 20
-      }, {
-        id: "David",
-        L: 23,
-        S: 20,
-        C: 81,
-        W: 15
-      }, {
-        id: "Jean",
-        L: 25,
-        S: 20,
-        C: 80,
-        W: 61
-      }, {
-        id: "Neil",
-        L: 20,
-        S: 20,
-        C: 80,
-        W: 20
-      }, {
-        id: "Jack",
-        L: 20,
-        S: 20,
-        C: 80,
-        W: 20
-      }, {
-        id: "Stephanie",
-        L: 20,
-        S: 20,
-        C: 80,
-        W: 20
-      }, {
-        id: "David",
-        L: 20,
-        S: 20,
-        C: 80,
-        W: 20
-      }, {
-        id: "Matt",
-        L: 20,
-        S: 20,
-        C: 80,
-        W: 20
-      }, {
-        id: "Ken",
-        L: 80,
-        S: 20,
-        C: 80,
-        W: 20
-      }, {
-        id: "Tom",
-        L: 50,
-        S: 20,
-        C: 80,
-        W: 20
-      }, {
-        id: "Jane",
-        L: 20,
-        S: 60,
-        C: 80,
-        W: 20
-      }, {
-        id: "Mary",
-        L: 20,
-        S: 40,
-        C: 80,
-        W: 20
-      }, {
-        id: "Mark",
-        L: 30,
-        S: 20,
-        C: 30,
-        W: 20
-      }
-    ]
-  )
